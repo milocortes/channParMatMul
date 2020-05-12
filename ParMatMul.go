d1476cc69bbb113f8b16e54d2c579101b68af8c3
milocortes <hermilocg@domain.com>
2020-05-11 20:40:53 -0500

    Se modifica md y programa

--- ParMatMul.go
+++ ParMatMul.go
@@ -1,82 +1,122 @@
 package main
 
-import (
-	"fmt"
-	)
-
-func main() {
-
-	// Definimos nuestros canales
-	norte:=make(chan int, 0)
-	este:=make(chan int, 0)
-	oeste:=make(chan int, 0)
-
-	// Definimos la matriz que posmultiplica
-	var matB=[][]int{{1,0,1},{0,1,0},{2,2,0}}
-	// Definimos la matriz que posmultiplica
-	var matA=[][]int{{1,2,3},{4,5,6},{7,8,9}}
+import "fmt"
 
 
-	// Definimos un slice que agregará los resultados de las multiplicaciones
-	var resultUno []int
+/*
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Definimos el proceso Zero que inicializa la suma parcial como una función que recibe un tipo de canal
+que es send-only de int (dado que chan<-int).
+*/
 
-	for i := 0; i <=2; i++ {
-		resultUno=append(resultUno,realizarMul(matA[0][:],matB[:][i], este, norte, oeste))
-	}
-
-	fmt.Println(resultUno)
-
-	// Definimos un slice que agregará los resultados de las multiplicaciones
-	var resultDos []int
-
-	for i := 0; i <=2; i++ {
-		resultDos=append(resultDos,realizarMul(matA[1][:],matB[:][i], este, norte, oeste))
-	}
-
-	fmt.Println(resultDos)
-
-	// Definimos un slice que agregará los resultados de las multiplicaciones
-	var resultTres []int
-
-	for i := 0; i <=2; i++ {
-		resultTres=append(resultTres,realizarMul(matA[2][:],matB[:][i], este, norte, oeste))
-	}
-
-	fmt.Println(resultTres)
-
+func zero(entero int,out chan<-int)  {
+	out<-entero
+	//close(out)
 }
 
-func multiplier(entero int,este chan int, norte chan int, oeste chan int )  {
-	go func() {
-		oeste<-(entero*(<-norte))+(<-este)
-	}()
+/*
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Definimos el proceso multiplier que recibe una suma parcial del este, y agrega a esta el resultado de
+multiplicar su elemento por el valor recibido del norte. El resultado es enviado por un canal al oestr.
+Además, el proceso envía al sur sobre un canal el valor recibido del norte.
+
+Este proceso lo definimos como una función que recibe dos tipos de canales, "oeste" y "sur",
+que son send-only de int (dado que chan<-int) y dos tipos de canales, "este" y "norte" , que son
+read-only de int (dado que <-chan int). Además, recibe un entero el cual es su elemento.
+*/
+
+func multiplier(firstElement int,este <-chan int, norte <-chan int, oeste chan<- int , sur chan<- int  )  {
+
+		secondElement:=(<-norte)
+		sum:=(<-este)
+
+		sum=(firstElement*(secondElement))+(sum)
+
+		oeste<-sum
+		sur<-secondElement
+		//close(oeste)
 }
 
-func actulizarCanal(entero int, canal chan int)  {
-	go func() {
-		canal<-entero
-	}()
+/*
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Definimos el proceso Source el cual es una función que recibe un entero y un tipo de canal que
+es send-only de int (dado que chan<-int). Envía el valor recibido al canal recibido.
+*/
+
+func source(entero int,out chan <- int)  {
+		out<-entero
+		//close(out)
 }
 
-func realizarMul(vectorA []int,vectorB []int,este chan int, norte chan int, oeste chan int ) int {
+func main()  {
 
-	// Inicializamos los valores de los canales
-	actulizarCanal(vectorB[2], norte)
-	actulizarCanal(0, este)
+	cz13:=make(chan int, 0)
+	cz26:=make(chan int, 0)
+	cz39:=make(chan int, 0)
 
-	multiplier(vectorA[2], este,norte,oeste )
+	c23:=make(chan int, 0)
+	c12:=make(chan int, 0)
+	c56:=make(chan int, 0)
+	c45:=make(chan int, 0)
+	c89:=make(chan int, 0)
+	c78:=make(chan int, 0)
 
-	// Actualizamos los valores de los canales
-	actulizarCanal(<-oeste, este)
-	actulizarCanal(vectorB[1], norte)
+	cr11:=make(chan int, 0)
+	cr24:=make(chan int, 0)
+	cr37:=make(chan int, 0)
 
-	multiplier(vectorA[1], este,norte,oeste )
+	c14:=make(chan int, 0)
+	c25:=make(chan int, 0)
+	c36:=make(chan int, 0)
+	c47:=make(chan int, 0)
+	c58:=make(chan int, 0)
+	c69:=make(chan int, 0)
+	
+	sink1:=make(chan int, 0)
+	sink2:=make(chan int, 0)
+	sink3:=make(chan int, 0)
 
-	// Actualizamos los valores de los canales
-	actulizarCanal(<-oeste, este)
-	actulizarCanal(vectorB[0], norte)
+	s1:=make(chan int, 0)
+	s2:=make(chan int, 0)
+	s3:=make(chan int, 0)
 
-	multiplier(vectorA[0], este,norte,oeste )
-	return(<-oeste)
+	var vecSoureUno=[]int{1,0,2}
+	var vecSoureDos=[]int{0,1,2}
+	var vecSoureTres=[]int{1,0,0}
+
+	var resultadosUno=[]int{}
+	var resultadosDos=[]int{}
+	var resultadosTres=[]int{}
+
+	for i := 0; i <=2; i++ {
+
+		go zero(0,cz13)
+		go zero(0,cz26)
+		go zero(0,cz39)
+
+		go source(vecSoureUno[i],s1)
+		go source(vecSoureDos[i],s2)
+		go source(vecSoureTres[i],s3)
+
+		go multiplier(3, cz13, s3, c23,c36)
+		go multiplier(2, c23, s2, c12,c25)
+		go multiplier(1, c12, s1, cr11,c14)
+
+		go multiplier(6, cz26, c36, c56,c69)
+		go multiplier(5, c56, c25, c45,c58)
+		go multiplier(4, c45, c14, cr24,c47)
+
+		go multiplier(9, cz39, c69, c89,sink3)
+		go multiplier(8, c89, c58, c78,sink2)
+		go multiplier(7, c78, c47, cr37,sink1)
+
+		resultadosUno=append(resultadosUno,<-cr11)
+		resultadosDos=append(resultadosDos,<-cr24)
+		resultadosTres=append(resultadosTres,<-cr37)
+	}
+
+	fmt.Println(resultadosUno)
+	fmt.Println(resultadosDos)
+	fmt.Println(resultadosTres)
 
 }
--- README.md
+++ README.md
@@ -118,6 +118,80 @@ func main() {
 
 
 ```
+#### Unidirectional Channel Types
+ 
+ En la medida que los programas crecen, es natural romper funciones grandes en piezas pequeñas. En los ejemplos anteriores, usamos tres goroutines que se comunican sobre dos canales, los cuales eran variables locales del ```main```.  Naturalmente, el programa se divide en tres funciones:
+ 
+``` go
+func counter(out chan int)
+func squarer(out, in chan int)
+func printer(in chan int)
+```
 
+La función ```squarer```, situada en medio del pipeline, toma dos parámetros, el intput del canal y el output del canal. Ambos tienen el mismo tipo, pero sus usos pretendidos son opuestos: ```in``` es sólo para recibir y ```out``` es sólo para enviar. Los nombre ```in``` y ```out``` comunican esta intención, pero a pesar de ello, nada previene que ```squarer``` envíe a ```in``` o reciba de ```out```.
+
+Este acuerdo es típico. Cuando se suministra un canal como parámetro de una función, es casi siempre con la intención que este sea usado exclusivamente ya sea exclusivamente para enviar o para recibir.
+
+Para documentar esta intención y prevenir su mal uso, Go proporciona tipos de canales ***unidireccionales*** que presentan sólo una de las operaciones de envío y recepción. El tipo  ```chan<-int```, un un canal ***send-only*** de ```int```, permite enviar pero no recibir. Por el contrario, el tipo ```<-chan int```, un canal ***receive-only*** de ```int```, permite recibir, pero no enviar.  La violación de este orden es detectado en tiempo de compilación.
+
+Dado que la operación ```close``` asegura que no ocurrirán más envíos en el canal, sólo la goroutine de envío está en posición de llamarla, y por esta razón este es un error de tiempo de compilación al intentar cerrar un canal exclusivo de recepción.
+
+Presentamos el programa nuevamente, incorporando el tipo de canales unidireccionales:
+
+```go
+package main
+
+import "fmt"
+
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+	go counter(naturals)
+	go squarer(squares, naturals)
+	printer(squares)
+
+```
+
+La llamada ```counter(naturals)``` implicitamente convierte ```naturals```, un valor de tipo ```chan int```,  al tipo del parámetro, ```chan<-int```. La llamada a ```printet(squares)```  realiza una conversión implícita similar a ```<-chan int```.  Las conversiones de tipos de canales bidireccionales a unidireccionales son permitidas en cualquier asignación. Sin embargo, no hay vuelta atrás: una vez que tienes un valor de un tipo unidireccional tal como ```chan<-int```, no hay forma de obtener de este un valor de tipo ```chan int``` que refiera a la misma estructura de datos del canal.
+
+#### Multiplicación Paralela de Matrices
+
+Abajo presentamos una multiplicación de matrices:
+
+![ ](images/matMul.png  "Multiplicación de matrices")
+
+Podemos expresar este cálculo usando 21 procesadoes, cada uno ejecutando un proceso sencillo. Los canales de un sólo sentido son denitados como flechas. Usaremos direcciones geográficas para denotar los vecinos de cada procesador.
+
+En términos generales, necesitamos n^2 + 4 X n procesadores para una multiplicación de matrices n X n
+Cada procesador ejecuta un sólo proceso. Un proceso es asignado a cada elemento de la matriz que premultiplica
+y los inicializamos con los valores de sus elementos. El proceso ```Source``` es inicializado con los vectores filas
+de la matriz que posmultiplica y los elementos son enviados uno a uno al proceso ```Multiplier``` hacia el sur; ellos
+a su vez transmiten los elementos hasta que finalmente son cachados por el proceso ```Sink```. El proceso ```multiplier```
+recibe una suma parcial del este, y le suma el resultado de multiplicar su elemento y el valor recibido del norte.
+La suma parcial es inicializada por el proceso ```Zero``` y estas finalmente se envían al proceso ```Result``` al este
+
+A excepción del proceso ```Multiplier```, el algoritmo del resto de los procesos es tribial. El proceso ```Zero``` ejecuta ```West<-0```
+tres veces  para inicializar la variable ```Sum``` del proceso ```Multiplier```; el proceso ```Source``` ejecuta ```Sout<-Vector[i]``` por cada
+uno de los tres elementos de las filas de la matriz que posmultiplica; el proceso ```Sink``` ejecuta ```North->dummy``` tres veces
+e ignora el valor recibido; el proceso ```Result``` ejecuta ```East->result``` e imprime o de otra forma usa el valor recibido.
  
- 
+En la siguiente imagen observamos una figura del acomodo de procesos y canales para la multiplicación de matrices:
+
+![ ](images/matChanelImage.png  "Arreglo de procesos para la multiplicación de matrices")
